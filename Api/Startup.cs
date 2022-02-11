4ae12726388b671cef369e78278313f0aa7ca7ca
Le Huu Hieu <01643815296h@gmail.com>
2022-02-11 11:08:42 +0700

    turn off hosting environment

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -54,12 +54,12 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+            // }
 
             app.UseHttpsRedirection();
 
