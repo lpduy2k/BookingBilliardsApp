4de75866e0f78e92cdd18093d5e80a529ef5d4de
Le Huu Hieu <01643815296h@gmail.com>
2022-03-14 23:47:34 +0700

    hieu-BAM-26

---
+++ Mobile/lib/repository/impl/auth_rep_impl.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:booking_billiards_app/configs/toast/toast.dart';
+import 'package:booking_billiards_app/model/response/sign_in_res.dart';
+import 'package:booking_billiards_app/model/request/sign_in_req.dart';
+import 'package:booking_billiards_app/repository/auth_rep.dart';
+import 'package:dio/dio.dart';
+
+class AuthRepImpl implements AuthRepo {
+  @override
+  Future<SignInRes> postSignIn(String url, SignInReq req) async {
+    var result = SignInRes();
+    try {
+      print(req.toJson());
+      Response response = await Dio().post(url + "/login", data: req.toJson());
+      result = SignInRes.signInResFromJson(jsonEncode(response.data));
+    } on DioError catch (e) {
+      showToastFail(e.response?.data["message"]);
+    }
+    return result;
+  }
+}
--- Mobile/lib/view_model/url_api/url_api.dart
+++ Mobile/lib/view_model/url_api/url_api.dart
@@ -1,5 +1,7 @@
 class UrlApi {
-  static const String serverPath = "https://bookingbida.azurewebsites.net/api/v1.0/";
+  static const String serverPath =
+      "https://bookingbida.azurewebsites.net/api/v1.0/";
   static const String bidaClubPath = '$serverPath/bidaclub';
   static const String bidaTablePath = '$serverPath/bidatable';
+  static const String userPath = '$serverPath/user';
 }
