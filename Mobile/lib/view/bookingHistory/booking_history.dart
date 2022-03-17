b6ab37308b9eba44be264b84611aaac7acc802b8
voxquoocshuyy <66914322+voxquoocshuyy@users.noreply.github.com>
2022-03-17 14:57:12 +0700

    demo

--- Mobile/lib/view/bookingHistory/booking_history.dart
+++ Mobile/lib/view/bookingHistory/booking_history.dart
@@ -1,5 +1,6 @@
 import 'package:booking_billiards_app/configs/themes/app_color.dart';
 import 'package:booking_billiards_app/utils/window_size.dart';
+import 'package:booking_billiards_app/view/detailsClub/detailsClub.dart';
 import 'package:booking_billiards_app/widgets/button/button.dart';
 import 'package:flutter/material.dart';
 
@@ -11,139 +12,143 @@ class BookingHistory extends StatefulWidget {
 }
 
 class _BookingHistoryState extends State<BookingHistory> {
-  
   @override
   Widget build(BuildContext context) {
-    
-    double windowWidth = MediaQuery.of(context).size.width;
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: windowWidth * windowSizeWidth(1),
-      ),
-      color: AppColor.white,
-      child: Scaffold(
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
         backgroundColor: AppColor.lightGrey,
-        body: const SingleChildScrollView(
-          child: Body(),
-        ),
-      ),
-    );
-  }
-}
-
-class Body extends StatelessWidget {
-  const Body({
-    Key? key,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    double windowWidth = MediaQuery.of(context).size.width;
-    double windowHeight = MediaQuery.of(context).size.height;
-
-    return Container(
-      // padding: EdgeInsets.only(
-      //   top: windowHeight * windowSizeHeight(70),
-      //   bottom: windowHeight * windowSizeHeight(35),
-      // ),
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height,
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: windowHeight * windowSizeHeight(5),
-              bottom: windowHeight * windowSizeHeight(15),
-            ),
-          ),
-          ListTile(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            title: const Text(
-              'Booking History',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-
-            // tileColor: AppColor.white,
-            iconColor: AppColor.black,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10),
-          ),
-          SizedBox(
-                  height: windowHeight * windowSizeHeight(100),
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: [
-                      Card(
-                        elevation: 8,
-                        child: ListTile(
-                          leading: Image.network(
-                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCR-s1OFav5Qn1MIUjAp3VE1FFIgohqJuauA&usqp=CAU',
-                            width: windowWidth * windowSizeWidth(50),
-                          ),
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(
-                            Icons.sports_golf,
-                            color: AppColor.black,
-                          ),
-                              Text(
-                                "ten quan",
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Icon(
-                            Icons.price_check,
-                            color: AppColor.black,
-                          ),
-                              Text(
-                                "tien",
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
-                          subtitle: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(
-                            Icons.tablet,
-                            color: AppColor.black,
-                          ),
-                              Text(
-                                "ten ban",
-                                style: const TextStyle(fontSize: 10),
-                              ),
-                              Icon(
-                            Icons.access_time_filled,
-                            color: AppColor.black,
-                          ),
-                              Text(
-                                "time",
-                                style: const TextStyle(fontSize: 14),
-                              ),
-                            ],
-                          ),
-                          
-                          
-                        ),
-                      )
-                    ],
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(
+                0, size.height * 0.05, 0, size.height * 0.05),
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                      child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.black87,
+                    ),
+                    onPressed: () {
+                      Navigator.popAndPushNamed(context, "/home");
+                    },
+                  )),
+                ]), // Booking Active
+                const Text(
+                  "Booking List",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700, fontSize: 18, height: 1.6),
+                ),
+                SizedBox(
+                  height: size.height * 0.005,
+                ),
+                Card(
+                  // color: AppColor.lightGreen,
+                  shape: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColor.grey, width: 1),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                )
-        ],
-      ),
-    );
+                  child: Column(children: [
+                    ListTile(
+                      title: Container(
+                        // padding: EdgeInsets.fromLTRB(
+                        //     0, size.height * 0.01, 0, size.height * 0.0001),
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.network(
+                              'https://media.istockphoto.com/photos/3d-rendering-of-an-isolated-billiard-table-in-a-top-view-with-a-full-picture-id945650288?k=20&m=945650288&s=170667a&w=0&h=p0tphNgA9OnGaOGYpwDYKE2MPV5SjmIkgupkmD6TOpE=',
+                              width: 150,
+                              height: 100,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'Total price',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 15,
+                                      height: 1.6),
+                                ),
+                                Text(
+                                  '100.000',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12,
+                                      height: 1.6),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      subtitle: Container(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Column(children: [
+                          Container(
+                            child: const Divider(
+                              height: 2,
+                              thickness: 1,
+                              indent: 0,
+                              endIndent: 0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Tên Quán',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 15,
+                                        height: 1.6),
+                                  ),
+                                  Text(
+                                    'Tên Bàn',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 12,
+                                        height: 1.6),
+                                  )
+                                ],
+                              ),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const DetailsClub()),
+                                    );
+                                  },
+                                  child: const Text(
+                                    "Detail",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 14.0,
+                                    ),
+                                  ))
+                            ],
+                          )
+                        ]),
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
