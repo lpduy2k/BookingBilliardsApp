0c1b81cef14cdc616932a980359fdb05e74d03df
DuyLP <lp.duy2k@gmail.com>
2022-03-23 18:46:53 +0700

    done

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:booking_billiards_app/configs/routes/routes_generator.dart';
 import 'package:booking_billiards_app/view_model/providers/main_providers/main_providers.dart';
+import 'package:booking_billiards_app/view_model/url_api/notification_api.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,9 +11,21 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    NotificationApi.init();
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
